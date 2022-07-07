57c515ad6a0445254aff1cebb6c90bcf793e2451
Ekeh Miracle Ihechi Tabitha <106749753+Miroyal1@users.noreply.github.com>
2022-07-07 23:04:11 +0100

    goodness

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,39 +1,20 @@
 #include "main.h"
 
-
-
 /**
+ * _isdigit - check for digit
  *
- *  * _isdigit - check for digit
+ * @c: integer
  *
- *   *
- *
- *    * @c: integer
- *
- *     *
- *
- *      * Return: 1 if it is a digit
- *
- *       */
-
+ * Return: 1 if it is a digit
+ */
 int _isdigit(int c)
-
 {
-
-		if (c >= 48 && c <= 57)
-
-				{
-
-							return (1);
-
-								}
-
-			else
-
-					{
-
-								return (0);
-
-									}
-
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
