21b4f724279f4a73db744e54be7d248a1b265306
Ekeh Miracle Ihechi Tabitha <106749753+Miroyal1@users.noreply.github.com>
2022-07-07 10:43:05 +0100

    milight

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,27 +1,13 @@
-
-
 #include "main.h"
-
 /**
- *
- *  *  * add - add two integers
- *
- *   *   * @a: whole
- *
- *    *    * @b: whole
- *
- *     *     *
- *
- *      *      *
- *
- *       *       * Return: a + b
- *
- *        */
-
+ *  * add - add two integers
+ *   * @a: whole
+ *    * @b: whole
+ *     *
+ *      *
+ *       * Return: a + b
+ */
 int add(int a, int b)
-
 {
-
 	return (a + b);
-
 }
