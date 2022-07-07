285d8a51bf7b7ec4fabddaea9efb02b2f259429f
Ekeh Miracle Ihechi Tabitha <106749753+Miroyal1@users.noreply.github.com>
2022-07-08 00:28:40 +0100

    relaxation weekend

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,45 +1,23 @@
 #include <stdio.h>
 
-
-
 /**
+ * main - prime factor
  *
- *  * main - prime factor
- *
- *   *
- *
- *    * Return: 0
- *
- *     */
-
+ * Return: 0
+ */
 int main(void)
-
 {
+	long int n, pf;
 
-		long int n, pf;
-
-
-
-			n = 612852475143;
-
-				for (pf = 2; pf <= n; pf++)
-
-						{
-
-									if (n % pf == 0)
-
-												{
-
-																n /= pf;
-
-																			pf--;
-
-																					}
-
-										}
-
-					printf("%ld\n", pf);
-
-						return (0);
-
+	n = 612852475143;
+	for (pf = 2; pf <= n; pf++)
+	{
+		if (n % pf == 0)
+		{
+			n /= pf;
+			pf--;
+		}
+	}
+	printf("%ld\n", pf);
+	return (0);
 }
